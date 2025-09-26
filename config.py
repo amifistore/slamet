9692484cd81b683818761bcbd8d1d4e74c874b81
amifistore <storeamifi@gmail.com>
2025-09-26 07:30:56 +0700

    Update config.py

--- config.py
+++ config.py
@@ -4,12 +4,14 @@ with open("config.json") as f:
     cfg = json.load(f)
 
 TOKEN = cfg["TOKEN"]
-ADMIN_IDS = cfg["ADMIN_IDS"]          # List of admin user_ids (int)
+ADMIN_IDS = [int(x) for x in cfg["ADMIN_IDS"]]  # pastikan tipe integer
 API_KEY = cfg["API_KEY"]
-BASE_URL = "https://panel.khfy-store.com/api_v2/"
-BASE_URL_AKRAB = "https://panel.khfy-store.com/api_v3/"
+BASE_URL = cfg["BASE_URL"]
+BASE_URL_AKRAB = cfg.get("BASE_URL_AKRAB", "")
 SALDO_FILE = 'saldo.json'
 RIWAYAT_FILE = 'riwayat_transaksi.json'
 HARGA_PRODUK_FILE = 'harga_produk.json'
 TOPUP_FILE = 'topup_user.json'
-QRIS_STATIS = cfg["QRIS_STATIS"]      # QRIS statis string dari merchant
+QRIS_STATIS = cfg["QRIS_STATIS"]
+WEBHOOK_URL = cfg.get("WEBHOOK_URL", "")
+WEBHOOK_PORT = cfg.get("WEBHOOK_PORT", 5000)
